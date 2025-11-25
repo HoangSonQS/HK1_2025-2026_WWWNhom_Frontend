aa82d3f2a8625727e130ad6c845815c42ec181f5
trieu22676191 <trieu22676191@gmail.com>
2025-11-26 00:40:03 +0700

    Thêm 2 nút Mua ngay và Thêm vào giỏ vào chi tiết sách

--- src/pages/BookDetailPage.jsx
+++ src/pages/BookDetailPage.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Layout, Card, Spin, Button, message, Empty, Tag } from 'antd';
+import { Layout, Card, Spin, Button, message, Empty, Tag, Space } from 'antd';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import { getBookById } from '../features/book/api/bookService';
 import { getImageUrl } from '../utils/imageUtils';
 import Header from '../components/Header';
+import BuyNowButton from '../components/BuyNowButton';
+import AddToCartButton from '../components/AddToCartButton';
 import '../styles/books.css';
 
 const { Content } = Layout;
@@ -106,6 +108,12 @@ const BookDetailPage = () => {
                             <div className="book-detail-quantity">
                                 <strong>Số lượng còn lại:</strong> {book.quantity}
                             </div>
+                        </div>
+                        <div style={{ marginTop: 24 }}>
+                            <Space direction="vertical" style={{ width: '300px' }} size="middle">
+                                <BuyNowButton book={book} size="large" block />
+                                <AddToCartButton book={book} size="large" block />
+                            </Space>
                         </div>
                     </div>
                 </div>
